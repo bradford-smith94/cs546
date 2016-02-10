15f41b53d9f7524b13b10b7ae48fb71da84f7cac
Bradford Smith <bsmith8@stevens.edu>
2016-02-10 16:27:25 -0500

    Assignment1: strings progress
    Both substring occuranceOf methods should work, just need to do the randomize
    paragraph method.

--- Assignments/Assignment1/string.js
+++ Assignments/Assignment1/string.js
@@ -1,6 +1,6 @@
 /* Bradford Smith (bsmith8)
  * CS 546 Assignment 1 string.js
- * 02/09/2016
+ * 02/10/2016
  * "I pledge my honor that I have abided by the Stevens Honor System."
  */
 
@@ -8,15 +8,34 @@ var m = {};
 
 //count and return how many times a substring occurs in a main string
 m.occurencesOfSubstring = function (main, substr) {
+    if (typeof main !== 'string' && !(main instanceof String) ||
+            typeof substr !== 'string' && !(substr instanceof String))
+        throw "Input Not of Type String: " + main + ", " + substr;
+
+    var count = 0;
+    var index = 0;
+    while (main.includes(substr, index)) {
+        index = main.indexOf(substr, index) + 1;
+        count++;
+    }
+    return count;
 }
 
 //count and return how many times a substring occurs in a main string
 //(Case insensitive)
 m.occurencesOfSubstringInsensitive = function (main, substr) {
+    if (typeof main !== 'string' && !(main instanceof String) ||
+            typeof substr !== 'string' && !(substr instanceof String))
+        throw "Input Not of Type String: " + main + ", " + substr;
+
+    return m.occurencesOfSubstring(main.toLowerCase(), substr.toLowerCase());
 }
 
 //given a string representing a paragraph, reorder the sentences
 m.randomizeSentences = function (paragraph) {
+    if (typeof paragraph !== 'string' && !(paragraph instanceof String))
+        throw "Input Not of Type String: " + paragraph;
+
 }
 
 module.exports = m;
