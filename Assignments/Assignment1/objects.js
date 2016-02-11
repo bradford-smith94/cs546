115936f61daf3e89ea458939af0de64318fc1911
Bradford Smith <bsmith8@stevens.edu>
2016-02-11 13:53:21 -0500

    Assignment1: Preliminary Objects module
    Also added in a lot of testing code. More still needs to be added to `test.js`.

--- Assignments/Assignment1/objects.js
+++ Assignments/Assignment1/objects.js
@@ -1,6 +1,6 @@
 /* Bradford Smith (bsmith8)
  * CS 546 Assignment 1 objects.js
- * 02/09/2016
+ * 02/11/2016
  * "I pledge my honor that I have abided by the Stevens Honor System."
  */
 
@@ -9,11 +9,34 @@ var m = {};
 //return a 'shallow clone' of the baseObject
 //  objects inside baseObject are just copied, (1 layer deep)
 m.shallowClone = function (baseObject) {
+    if (baseObject === null || typeof(baseObject) !== 'object')
+        throw "Invalid Object";
+
+    var retObj = {};
+
+    for (var key in baseObject) {
+        retObj[key] = baseObject.key;
+    }
+
+    return retObj;
 }
 
 //return a 'deep clone' of the baseObject
 //  each object that is encountered nested in baseObject is also deeply cloned
 m.deepClone = function (baseObject) {
+    if (baseObject === null || typeof(baseObject) !== 'object')
+        throw "Invalid Object";
+
+    var retObj = {};
+
+    for (var key in baseObject) {
+        if (typeof(key) === 'object')
+            retObj[key] = m.deepClone(key);
+        else
+            retObj[key] = baseObject.key;
+    }
+
+    return retObj;
 }
 
 module.exports = m;
--- Assignments/Assignment1/test.js
+++ Assignments/Assignment1/test.js
@@ -1,3 +1,9 @@
+/* Bradford Smith (bsmith8)
+ * CS 546 Assignment 1 test.js
+ * 02/11/2016
+ * "I pledge my honor that I have abided by the Stevens Honor System."
+ */
+
 var strModule = require("./string.js"),
     numberModule = require("./numbers.js"),
     objectModule = require("./objects.js"),
@@ -5,3 +11,107 @@ var strModule = require("./string.js"),
     dateModule = require("./dates.js");
 
 console.log("All modules have loaded!");
+
+//result is boolean expression for test pass/fail
+//on test fail print given message
+var test = function(result, message) {
+    if (!result)
+        console.log(message);
+    return result;
+}
+
+function testStrings() {
+    var passed = 0;
+    var total = 0;
+    console.log("================================================================================");
+    console.log("Testing String module:");
+    try {
+        strModule.occurencesOfSubstring(null, "hi");
+        console.log("Failed: null input");
+    } catch (e) {
+        passed++;
+    }
+    total++;
+    if (test(0 == strModule.occurencesOfSubstring("", "something"),
+                "Failed: finding something in nothing")) {
+        passed++;
+    }
+    total++;
+    if (test(2 == strModule.occurencesOfSubstring("foofoofoo", "foofoo"),
+                "Failed: 2 overlapping occurences")) {
+        passed++;
+    }
+    total++;
+
+    console.log("String module passed: " + passed + " out of " + total + " tests.");
+}
+
+function testNumbers() {
+    var passed = 0;
+    var total = 0;
+    console.log("================================================================================");
+    console.log("Testing Numbers module:");
+    if (test(25 == numberModule.triangleArea(5, 10),
+                "Failed: area of triangle")) {
+        passed++;
+    }
+    total++;
+    try {
+        numberModule.triangleArea(-1, -5);
+        console.log("Failed: negative inputs");
+    } catch (e) {
+        passed++;
+    }
+    total++;
+
+    console.log("Numbers module passed: " + passed + " out of " + total + " tests.");
+}
+
+function testObjects() {
+    var passed = 0;
+    var total = 0;
+    console.log("================================================================================");
+    console.log("Testing Object module:");
+
+    try {
+        objectModule.shallowClone(null);
+        console.log("Failed: shallowClone null");
+    } catch (e) {
+        passed++;
+    }
+    total++;
+    try {
+        objectModule.deepClone(null);
+        console.log("Failed: deepClone null");
+    } catch (e) {
+        passed++;
+    }
+    total++;
+
+    console.log("Object module passed: " + passed + " out of " + total + " tests.");
+}
+
+function testArrays() {
+    var passed = 0;
+    var total = 0;
+    console.log("================================================================================");
+    console.log("Testing Arrays module:");
+
+    console.log("Arrays module passed: " + passed + " out of " + total + " tests.");
+}
+
+function testDates() {
+    var passed = 0;
+    var total = 0;
+    console.log("================================================================================");
+    console.log("Testing Dates module:");
+
+    console.log("Dates module passed: " + passed + " out of " + total + " tests.");
+}
+
+//run tests
+testStrings();
+testNumbers();
+testObjects();
+testArrays();
+testDates();
