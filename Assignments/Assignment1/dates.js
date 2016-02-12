8d969f4c931e99b964bb229798076d829a1d86c3
Bradford Smith <bsmith8@stevens.edu>
2016-02-12 00:05:08 -0500

    Assignment1: Preliminary Dates module
    Still need to implement `nextFridayTheThirteenth()`. Also more testing needs to
    be done.

--- Assignments/Assignment1/dates.js
+++ Assignments/Assignment1/dates.js
@@ -1,6 +1,6 @@
 /* Bradford Smith (bsmith8)
  * CS 546 Assignment 1 dates.js
- * 02/09/2016
+ * 02/12/2016
  * "I pledge my honor that I have abided by the Stevens Honor System."
  */
 
@@ -8,18 +8,43 @@ var m = {};
 
 //return the number of days between the current date and someDate
 m.daysUntil = function (someDate) {
+    if (!(someDate instanceof Date))
+        throw "Input not of Type Date";
+
+    var today = new Date();
+    if ((someDate.getTime() - today.getTime()) < 0)
+        throw "Date is in the past";
+
+    var day = 24*60*60*1000; //hours*minutes*seconds*milliseconds
+    return Math.round((someDate.getTime() - today.getTime()) / day);
 }
 
 //return the number of days left in the year
 m.daysLeftInYear = function () {
+    var today = new Date();
+    var endOfYear = new Date(today.getYear(), 11, 31); //december 31 of the current year
+    var day = 24*60*60*1000; //hours*minutes*seconds*milliseconds
+
+    return Math.round((endOfYear.getTime() - today.getTime()) / day);
 }
 
 //return the number of days that have passed since someDate
 m.daysSince = function (someDate) {
+    if (!(someDate instanceof Date))
+        throw "Input not of Type Date";
+
+    var today = new Date();
+    if ((today.getTime() - someDate.getTime()) < 0)
+        throw "Date is in the future";
+
+    var day = 24*60*60*1000; //hours*minutes*seconds*milliseconds
+    return Math.round((today.getTime() - someDate.getTime()) / day);
 }
 
 //return the next date that is both a Friday and the 13th
 m.nextFridayTheThirteenth = function () {
+    var today = new Date();
+    var day = 24*60*60*1000; //hours*minutes*seconds*milliseconds
 }
 
 module.exports = m;
--- Assignments/Assignment1/test.js
+++ Assignments/Assignment1/test.js
@@ -1,6 +1,6 @@
 /* Bradford Smith (bsmith8)
  * CS 546 Assignment 1 test.js
- * 02/11/2016
+ * 02/12/2016
  * "I pledge my honor that I have abided by the Stevens Honor System."
  */
 
@@ -20,6 +20,7 @@ var test = function(result, message) {
     return result;
 }
 
+//do all the tests for the strings.js module
 function testStrings() {
     var passed = 0;
     var total = 0;
@@ -27,18 +28,18 @@ function testStrings() {
     console.log("Testing String module:");
     try {
         strModule.occurencesOfSubstring(null, "hi");
-        console.log("Failed: null input");
+        console.log("[Failed!]: null input");
     } catch (e) {
         passed++;
     }
     total++;
     if (test(0 == strModule.occurencesOfSubstring("", "something"),
-                "Failed: finding something in nothing")) {
+                "[Failed!]: finding something in nothing")) {
         passed++;
     }
     total++;
     if (test(2 == strModule.occurencesOfSubstring("foofoofoo", "foofoo"),
-                "Failed: 2 overlapping occurences")) {
+                "[Failed!]: 2 overlapping occurences")) {
         passed++;
     }
     total++;
@@ -46,19 +47,20 @@ function testStrings() {
     console.log("String module passed: " + passed + " out of " + total + " tests.");
 }
 
+//do all the tests for the numbers.js module
 function testNumbers() {
     var passed = 0;
     var total = 0;
     console.log("================================================================================");
     console.log("Testing Numbers module:");
     if (test(25 == numberModule.triangleArea(5, 10),
-                "Failed: area of triangle")) {
+                "[Failed!]: area of triangle")) {
         passed++;
     }
     total++;
     try {
         numberModule.triangleArea(-1, -5);
-        console.log("Failed: negative inputs");
+        console.log("[Failed!]: negative inputs");
     } catch (e) {
         passed++;
     }
@@ -67,6 +69,7 @@ function testNumbers() {
     console.log("Numbers module passed: " + passed + " out of " + total + " tests.");
 }
 
+//do all the tests for the objects.js module
 function testObjects() {
     var passed = 0;
     var total = 0;
@@ -75,14 +78,14 @@ function testObjects() {
 
     try {
         objectModule.shallowClone(null);
-        console.log("Failed: shallowClone null");
+        console.log("[Failed!]: shallowClone null");
     } catch (e) {
         passed++;
     }
     total++;
     try {
         objectModule.deepClone(null);
-        console.log("Failed: deepClone null");
+        console.log("[Failed!]: deepClone null");
     } catch (e) {
         passed++;
     }
@@ -91,20 +94,79 @@ function testObjects() {
     console.log("Object module passed: " + passed + " out of " + total + " tests.");
 }
 
+//do all the tests for the arrays.js module
 function testArrays() {
     var passed = 0;
     var total = 0;
     console.log("================================================================================");
     console.log("Testing Arrays module:");
+    try {
+        arrayModule.shallowClone(null);
+        console.log("[Failed!]: shallowClone null");
+    } catch (e) {
+        passed++;
+    }
+    total++;
+    try {
+        arrayModule.randomized(null);
+        console.log("[Failed!]: randomized null");
+    } catch (e) {
+        passed++;
+    }
+    total++;
+    if (test(["1", "2", "3", "4"].toString() === arrayModule.shallowClone(["1", "2", "3", "4"]).toString(),
+                "[Failed!]: shallowClone")) {
+        passed++;
+    }
+    total++;
+    if (test(["1", "2", "3", "4"].toString() !== arrayModule.randomized(["1", "2", "3", "4"]).toString(),
+                "[Failed!]: randomized\n\t *note* there is a random chance that this is a false positive")) {
+        passed++;
+    }
+    total++;
 
     console.log("Arrays module passed: " + passed + " out of " + total + " tests.");
 }
 
+//do all the tests for the dates.js module
 function testDates() {
     var passed = 0;
     var total = 0;
     console.log("================================================================================");
     console.log("Testing Dates module:");
+    try {
+        dateModule.daysUntil(null);
+        console.log("[Failed!]: days until null");
+    } catch (e) {
+        passed++;
+    }
+    total++;
+    try {
+        dateModule.daysUntil(new Date(1990, 0, 1));
+        console.log("[Failed!]: days until past");
+    } catch (e) {
+        passed++;
+    }
+    total++;
+    if (test(0 == dateModule.daysUntil(new Date()),
+                "[Failed!]: days until today")) {
+        passed++;
+    }
+    total++;
+    try {
+        dateModule.daysSince(null);
+        console.log("[Failed!]: days since null");
+    } catch (e) {
+        passed++;
+    }
+    total++;
+    try {
+        dateModule.daysSince(new Date(2266, 0, 1));
+        console.log("[Failed!]: days since future");
+    } catch (e) {
+        passed++;
+    }
+    total++;
 
     console.log("Dates module passed: " + passed + " out of " + total + " tests.");
 }
