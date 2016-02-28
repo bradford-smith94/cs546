7a13bd402d9213094756eb10112217115bc3fcfb
Bradford Smith <bsmith8@stevens.edu>
2016-02-28 18:23:49 -0500

    Assignment2: finished functions and routes
    toDoEntries.js functions now perform correctly. API routes in server.js now
    perform correctly with toDoEntries.js.
    
    Still need to update the index.html to describe the routes.

--- Assignments/Assignment2/server.js
+++ Assignments/Assignment2/server.js
@@ -1,7 +1,13 @@
+/* Bradford Smith (bsmith8)
+ * CS 546 Assignment 2 server.js
+ * 02/28/2016
+ * "I pledge my honor that I have abided by the Stevens Honor System."
+ */
+
 // We first require our express package
 var express = require('express');
 var bodyParser = require('body-parser');
-var myData = require('./data.js');
+var todo = require('./toDoEntries.js');
 
 // This package exports the function to create an express instance:
 var app = express();
@@ -25,27 +31,89 @@ app.get("/home", function (request, response) {
 });
 
 app.get("/api/todo", function (request, response) {
+    response.json(todo.getEntries());
 });
 
 app.get("/api/todo/open", function (request, response) {
+    response.json(todo.getOpenEntries());
 });
 
 app.get("/api/todo/completed", function (request, response) {
+    response.json(todo.getCompletedEntries());
 });
 
 app.get("/api/todo/:id", function (request, response) {
+    try {
+        response.json(todo.getEntryById(request.params.id));
+    }
+    catch (e) {
+        response.status(404).json({error: "An entry with the ID of {" + request.params.id + "} could not be found"});
+    }
 });
 
 app.post("/api/todo", function (request, response) {
+    var author = request.body.author;
+    var title = request.body.taskTitle;
+    var descrip = request.body.taskDescription;
+
+    if ((author === undefined || author === null || author === "") ||
+            (title === undefined || title === null || title === "") ||
+            (descrip === undefined || descrip === null || descrip === "")) {
+        response.status(500).json({error: "You must provide valid information in the request body to create an entry."});
+    }
+    else {
+        try {
+            response.json(todo.createEntry(author, title, descrip));
+        }
+        catch (e) {
+            response.status(404).json({error: "An entry with the ID of {" + request.params.id + "} could not be found"});
+        }
+    }
 });
 
 app.put("/api/todo/:id", function (request, response) {
+    var title = request.body.taskTitle;
+    var descrip = request.body.taskDescription;
+    var status =  request.body.status;
+
+    if (((title === undefined || title === null || title === "") &&
+                (descrip === undefined || descrip === null || descrip === "") &&
+                (status === undefined || status === null || status === "")) ||
+            (status !== "open" && status !== "completed")) {
+        response.status(500).json({error: "You must provide valid information in the request body to create an entry."});
+    }
+    else {
+        try {
+            response.json(todo.updateEntry(request.params.id, title, descrip, status));
+        }
+        catch (e) {
+            response.status(404).json({error: "An entry with the ID of {" + request.params.id + "} could not be found"});
+        }
+    }
 });
 
 app.post("/api/todo/:id/notes", function (request, response) {
+    var note = request.body.note;
+    if (note === undefined || note === null || note === "")
+        response.status(500).json({error: "You must provide valid information in the request body to create an entry."});
+    else {
+        try {
+            response.json(todo.addNote(request.params.id, note));
+        }
+        catch (e) {
+            response.status(404).json({error: "An entry with the ID of {" + request.params.id + "} could not be found"});
+        }
+    }
 });
 
 app.delete("/api/todo/:id", function (request, response) {
+    try {
+        todo.deleteEntry(request.params.id);
+        response.json({success: true});
+    }
+    catch (e) {
+        response.status(404).json({error: "An entry with the ID of {" + request.params.id + "} could not be found"});
+    }
 });
 
 app.get("/", function (request, response) {
--- Assignments/Assignment2/toDoEntries.js
+++ Assignments/Assignment2/toDoEntries.js
@@ -1,6 +1,6 @@
 /* Bradford Smith (bsmith8)
  * CS 546 Assignment 2 toDoEntries.js
- * 02/25/2016
+ * 02/28/2016
  * "I pledge my honor that I have abided by the Stevens Honor System."
  */
 
@@ -20,7 +20,7 @@ var exports = module.exports = {};
 
 //object to hold `ID: ToDoEntry` pairs
 var entries = {};
-var id = 1;
+var id = 0;
 
 //return an array listing all ToDoEntries currently stored in memory
 exports.getEntries = function () {
@@ -59,21 +59,76 @@ exports.getCompletedEntries = function () {
 
 //return a single ToDoEntry that has the specified id
 exports.getEntryById = function (id) {
-    return entry[id];
+    try {
+        var entryToReturn = entries[id];
+        if (entryToReturn === undefined || entryToReturn === null)
+            throw "Entry was deleted!";
+        else
+            return entryToReturn;
+    }
+    catch (e) {
+        throw "Entry not found!";
+    }
 };
 
 //create and store a new ToDoEntry
+//return the newly created entry
 exports.createEntry = function (author, taskTitle, taskDescription) {
+    var newEntry = {};
+
+    newEntry.id = id++;
+    newEntry.author = author;
+    newEntry.taskTitle = taskTitle;
+    newEntry.taskDescription = taskDescription;
+    newEntry.taskNotes = [];
+    newEntry.status = "open";
+
+    entries[newEntry.id] = newEntry;
+
+    return entries[newEntry.id];
 };
 
 //update the ToDoEntry that has the specified id
-exports.updateEntry = function (id, author, taskTitle, taskDescription) {
+//return the updated entry
+exports.updateEntry = function (id, taskTitle, taskDescription, status) {
+    try {
+        var entryToUpdate = entries[id];
+    }
+    catch (e) {
+        throw "Entry not found!";
+    }
+
+    if (taskTitle !== undefined && taskTitle !== null && taskTitle !== "")
+        entryToUpdate.taskTitle = taskTitle;
+    if (taskDescription !== undefined && taskDescription !== null && taskDescription !== "")
+        entryToUpdate.taskDescription = taskDescription;
+    if (status !== undefined && status !== null && status !== "")
+        entryToUpdate.status = status;
+
+    return entries[id];
 };
 
 //update the ToDoEntry that has the specified by adding notes
-exports.addNotes = function (id, notes) {
+//return the updated entry
+exports.addNote = function (id, note) {
+    try {
+        var entryToUpdate = entries[id];
+    }
+    catch (e) {
+        throw "Entry not found!";
+    }
+
+    entryToUpdate.taskNotes.push(note);
+
+    return entries[id];
 };
 
 //delete the ToDoEntry that has the specified id
 exports.deleteEntry = function (id) {
+    try {
+        delete entries[id];
+    }
+    catch (e) {
+        throw "Entry not found!";
+    }
 };
