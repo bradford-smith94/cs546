0dd32f1ea81a9c3b3df8b2b452f175b19d661f24
Bradford Smith <bsmith8@stevens.edu>
2016-03-05 23:38:59 -0500

    Lab5: added ids to form elements
    These id's are stored in variables in the `validators.js` script.

--- Labs/Lab5/static/js/validators.js
+++ Labs/Lab5/static/js/validators.js
@@ -4,4 +4,12 @@
  * "I pledge my honor that I have abided by the Stevens Honor System."
  */
 
-
+//IIFE being passed jQuery global variable
+(function ($) {
+    var form1 = $("#ret-form");
+    var err1 = $("#ret-error-message");
+    var form2 = $("#inv-form");
+    var err2 = $("#inv-error-message");
+    var form3 = $("#loa-form");
+    var err3 = $("#loa-error-message");
+})(jQuery);
