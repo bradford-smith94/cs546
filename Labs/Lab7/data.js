7603776fe43307f6599973fa9e188afbb62d99d1
Bradford Smith <bsmith8@stevens.edu>
2016-03-21 14:34:20 -0400

    Lab7: Task 2
    Implemented `createComment` and `getAllComments` functions in `data.js`. Also
    added my submit script.
    
    Still need to update the routes in `server.js` to use `data.js`.

--- Labs/Lab7/data.js
+++ Labs/Lab7/data.js
@@ -1,3 +1,9 @@
+/* Bradford Smith (bsmith8)
+ * CS 546 Lab 7 data.js
+ * 03/21/2016
+ * "I pledge my honor that I have abided by the Stevens Honor System."
+ */
+
 var MongoClient = require('mongodb').MongoClient,
     settings = require('./config.js'),
     Guid = require('guid');
@@ -12,7 +18,9 @@ MongoClient.connect(fullMongoUrl)
         // setup your body
         exports.createComment = function(comment) {
             // you may return Promise.reject("error message"); if there is an error, such as a comment not being provided;
-        // this will result in a rejected promise immediately being returned
+            // this will result in a rejected promise immediately being returned
+            if (!comment || comment === "")
+                return Promise.reject("You must provide a comment!");
 
             // return a promise that resolves the new comment
             return myCollection.insertOne({ _id: Guid.create().toString(), comment: comment }).then(function(newDoc) {
@@ -21,7 +29,7 @@ MongoClient.connect(fullMongoUrl)
         };
 
         exports.getAllComments = function() {
-            // write the body here
             // return a promise that resolves to all the comments in your collection.
+            return myCollection.find().toArray();
         }
     });
--- Labs/Lab7/server.js
+++ Labs/Lab7/server.js
@@ -1,3 +1,9 @@
+/* Bradford Smith (bsmith8)
+ * CS 546 Lab 7 server.js
+ * 03/21/2016
+ * "I pledge my honor that I have abided by the Stevens Honor System."
+ */
+
 // We first require our express package
 var express = require('express');
 var bodyParser = require('body-parser');
@@ -13,17 +19,17 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
-// This middleware will activate for every request we make to 
+// This middleware will activate for every request we make to
 // any path starting with /assets;
-// it will check the 'static' folder for matching files 
+// it will check the 'static' folder for matching files
 app.use('/assets', express.static('static'));
 
 app.post("/", function(request, response) {
     var comment = request.body.comment;
 
     // In this route, you will call the createComment function for your data module
-    // and respond with the result of that promise            
-        // On error, do: 
+    // and respond with the result of that promise
+        // On error, do:
         // response.render("pages/home", { error: null, comments: ALL THE COMMENTS });
 
         // On success, do:
