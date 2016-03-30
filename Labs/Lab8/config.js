06afcbc6d1e7efee3108f18a40d015e140b82630
Bradford Smith <bsmith8@stevens.edu>
2016-03-30 15:56:05 -0400

    Lab8: cloned Lecture-9 as starting point
    Also updated the MongoDB configuration file to use `lab8` as the database and
    added a submit script.

---
+++ Labs/Lab8/config.js
@@ -0,0 +1,12 @@
+/* Bradford Smith (bsmith8)
+ * CS 546 Lab 8 config.js
+ * 03/30/2016
+ * "I pledge my honor that I have abided by the Stevens Honor System."
+ */
+
+var exports = module.exports = {};
+
+exports.mongoConfig = {
+    serverUrl: "mongodb://localhost:27017/",
+    database: "lab8"
+};
