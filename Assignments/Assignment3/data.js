28fac4d9b4cd826b27e82547708e39da7143f754
Bradford Smith <bsmith8@stevens.edu>
2016-04-05 22:52:39 -0400

    Assignment3: began adding database interaction
    Create user and getUserBySessionId should be finished.

--- Assignments/Assignment3/data.js
+++ Assignments/Assignment3/data.js
@@ -30,19 +30,41 @@ var exports = module.exports = {};
 
 MongoClient.connect(fullMongoUrl)
     .then(function(db) {
-        var myCollection = db.collection("users");
+        var userCollection = db.collection("users");
 
+        //create a user and add them to the database
         exports.createUser = function(username, hash) {
             //make sure to check for duplicate username, and existence of
             //password
+            if (!username || username === "")
+                return Promise.reject("You must provide a username!");
+            if (!hash || hash === "")
+                return Promise.reject("You must provide a password!");
+
+            return userCollection.insertOne({_id: Guid.create().toString(), username: username, encryptedPassword: hash, currentSessionId: Guid.create().toString(), profile: {} }).then(function(newUser) {
+                return newUser.insertedId;
+            });
         };
 
-        exports.editProfile = function() {
+        //update a user's profile
+        exports.editProfile = function(username, firstName, lastName, hobby, petName) {
         };
 
+        //try to get a user by sessionId
         exports.getUserBySessionId = function(sessionId) {
+            if (!sessionId)
+                return Promise.reject("You must provide a sessionId!");
+
+            return userCollection.find({currentSessionId: sessionId }).limit(1).toArray().then(function(listOfUsers) {
+                if (listOfUsers.length === 0)
+                    throw "Could not find user with currentSessionId " + sessionId;
+
+                return listOfUsers[0];
+            });
         };
 
+        //try to get a user by username and password only if the password
+        //matches
         exports.getUserByCredentials = function(username, password) {
             user = ""; //get from Mongo
             bcrypt.compare(password, user.encryptedPassword, function(err, res) {
