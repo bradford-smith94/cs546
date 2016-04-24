3a375a1dbc4d2948064a324ddbbfa8f0f346753a
Bradford Smith <bsmith8@stevens.edu>
2016-04-24 17:08:12 -0400

    Lab10: finished
    Added submit script.

---
+++ Labs/Lab10/submit.sh
@@ -0,0 +1,17 @@
+#! /bin/sh
+################################################################################
+# Bradford Smith (bsmith8)
+# CS 546 Lab 10 submit.sh
+# 04/24/2016
+# "I pledge my honor that I have abided by the Stevens Honor System."
+################################################################################
+
+AUTHOR='bsmith8'
+ASSIGNMENT='cs546-lab10'
+
+# make tar.gz archive for easy submission
+tar -cvzf $AUTHOR\_$ASSIGNMENT.tar.gz ./* --exclude='./node_modules' --exclude='*.zip'
+
+# or zip if you prefer
+#zip -r $AUTHOR\_$ASSIGNMENT.zip ./* -x './node_modules/*' '*.tar.gz'
+
