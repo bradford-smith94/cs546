e178eda91ee8ddd3fc98242ce3d059ae6183578d
Bradford Smith <bsmith8@stevens.edu>
2016-05-03 22:07:02 -0400

    Assignment4: cloned lecture 8 as starting point
    Also updated `data.js` to fix the guid package issue, and updated `config.js` to
    use hw4 as the database.

---
+++ Assignments/Assignment4/config.js
@@ -0,0 +1,12 @@
+/* Bradford Smith (bsmith8)
+ * CS 546 Assignment 4 config.js
+ * 05/03/2016
+ * "I pledge my honor that I have abided by the Stevens Honor System."
+ */
+
+var exports = module.exports = {};
+
+exports.mongoConfig = {
+    serverUrl: "mongodb://localhost:27017/",
+    database: "hw4"
+};
