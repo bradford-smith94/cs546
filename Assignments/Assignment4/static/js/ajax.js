8a7d87e712e079e6fa85237f6e6c9478bd1be5f4
Bradford Smith <bsmith8@stevens.edu>
2016-05-09 01:05:47 -0400

    Assignment4: added button and link handlers
    Ajax will now handler all button and link click events. Need to hookup button
    and link actions.

--- Assignments/Assignment4/static/js/ajax.js
+++ Assignments/Assignment4/static/js/ajax.js
@@ -1,6 +1,6 @@
 /* Bradford Smith (bsmith8)
  * CS 546 Assignment 4 ajax.js
- * 05/08/2016
+ * 05/09/2016
  * "I pledge my honor that I have abided by the Stevens Honor System."
  */
 
@@ -9,9 +9,45 @@
         movieTitle = $("#add-movie-title"),
         movieRating = $("#add-movie-rating");
 
+    //handler for all link clicks
+    $("a").click(function(event) {
+        event.preventDefault();
+
+        var linkHref = $(this).attr("href");
+
+        if (linkHref === "#all") {
+            //reload all movies
+            //TODO:
+        }
+        else if (linkHref === "#pop") {
+            //reload most popular movies
+            //TODO:
+        }
+    });
+
+    //handler for all button clicks
+    $("button").click(function() {
+        var btnClass = $(this).attr("class");
+        var btnId = $(this).attr("id");
+        var movId = btnId.substring(btnId.indexOf("_") + 1);
+
+        if (btnClass === "increment") {
+            //it's an increment button
+            //TODO:
+        }
+        else if (btnClass === "decrement") {
+            //it's a decrement button
+            //TODO:
+        }
+        else if (btnClass === "delete") {
+            //it's a delete button
+            //TODO:
+        }
+    });
+
+    //handler for form submission
     addMovieForm.submit(function(event) {
         event.preventDefault();
-        console.log("Click");
 
         var newTitle = movieTitle.val();
         var newRating = movieRating.val();
